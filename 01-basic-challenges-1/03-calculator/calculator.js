b26225e0967d5e5c76f72b2614fc307a64fc9b46
siranjeevi <ksiranjeevi92@gmail.com>
2026-01-04 19:27:58 +0530

    Calculator pbm

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,17 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  let result = null;
+  switch (operator) {
+    case "+":
+      return (result = num1 + num2);
+    case "-":
+      return (result = num1 - num2);
+    case "*":
+      return (result = num1 * num2);
+    case "/":
+      return (result = num1 / num2);
+    default:
+      throw new Error();
+  }
+}
 
 module.exports = calculator;
