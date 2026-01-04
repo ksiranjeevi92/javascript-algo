71f7eceaf1e66134e323da87c1f3f2a09ba6caaa
siranjeevi <ksiranjeevi92@gmail.com>
2026-01-04 19:47:48 +0530

    Char occurrences solution

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,13 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  let count = 0;
+  for (let c of str) {
+    if (c === char) {
+      count++;
+    }
+  }
+  return count;
+  //Functional
+  //   return [...str].filter((c) => c == char).length;
+}
 
 module.exports = countOccurrences;
