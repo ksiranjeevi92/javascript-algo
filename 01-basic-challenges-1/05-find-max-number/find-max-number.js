696be5b3a69d8678f2584166a248bfd13e1dda62
siranjeevi <ksiranjeevi92@gmail.com>
2026-01-11 17:16:49 +0530

    finished easy section

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,12 @@
-function findMaxNumber() {}
+function findMaxNumber(input) {
+  let max = input[0];
+
+  for (let i = 1; i < input.length; i++) {
+    if (input[i] > max) {
+      max = input[i];
+    }
+  }
+  return max;
+}
 
 module.exports = findMaxNumber;
